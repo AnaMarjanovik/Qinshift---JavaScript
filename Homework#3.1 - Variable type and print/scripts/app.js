d6c0c97dc5f50920794b0d63b7caacfc77ae1812
AnaMarjanovik <108395368+AnaMarjanovik@users.noreply.github.com>
2024-12-09 23:28:29 +0100

    Add files via upload

---
+++ Homework#3.1 - Variable type and print/scripts/app.js
@@ -0,0 +1,17 @@
+let objectVar = null;
+let booleanVar = true;
+let numberVar = 14;
+let stringVar = "Ana";
+let undefinedVar;
+
+
+function VariableType(variable){
+    console.log(typeof(variable));
+}
+
+
+VariableType(objectVar);
+VariableType(booleanVar);
+VariableType(numberVar);
+VariableType(stringVar);
+VariableType(undefinedVar);

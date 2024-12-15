e30fb0a88dd2cc668b410f70658c7c81042a26aa
AnaMarjanovik <108395368+AnaMarjanovik@users.noreply.github.com>
2024-12-15 22:36:53 +0100

    Add files via upload

---
+++ Homwork#4.1 - Function tellStory()/scripts/tellStory.js
@@ -0,0 +1,14 @@
+// function tellStory(array = ['Ana','happy','playing']){
+//     alert(`This is ${array[0]}. ${array[0]} is a nice person. Today they are ${array[1]}. They are ${array[2]} all day. The end.`);
+// }
+
+// tellStory();
+
+
+function tellStory(array = ['Ana','happy','playing']){
+    console.log(`This is ${array[0]}. ${array[0]} is a nice person. Today they are ${array[1]}. They are ${array[2]} all day. The end.`);
+}
+
+console.log(tellStory());
+
+

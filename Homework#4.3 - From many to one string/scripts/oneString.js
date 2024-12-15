ac9320a876fec6b37eb35f583f7959fe96c43aaf
AnaMarjanovik <108395368+AnaMarjanovik@users.noreply.github.com>
2024-12-15 22:37:31 +0100

    Add files via upload

---
+++ Homework#4.3 - From many to one string/scripts/oneString.js
@@ -0,0 +1,9 @@
+function final(array){
+    let add = '';
+    for(let i=0; i<array.length; i++){
+        add = add + ' ' + array[i] + '';
+    }
+    console.log(add);
+}
+
+final(['Hello','from', 'Qinshift', 'Academy','!']);
